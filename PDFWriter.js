945d2102425f6ae8d727a1e5e6b2f4597866c4a8
Thiago Cruz Machado <96290053+yTrosky@users.noreply.github.com>
2024-04-30 21:31:26 -0300

    Add files via upload

---
+++ PDFWriter.js
@@ -0,0 +1,13 @@
+var pdf = require("html-pdf");
+
+class PDFWriter{
+    static WritePDF(filename, html){
+        pdf.create(html, {}).toFile(filename, (err) => {
+            if(err){
+                console.log(err);
+            }
+        });
+    }
+}
+
+module.exports = PDFWriter;
